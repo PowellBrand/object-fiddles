8cf40ec01e50838f45d4e62d800bb2973162d0d9
PowellBrand <powellbrandon@outlook.com>
2017-11-28 14:14:39 -0700

    p1 done

--- practice.js
+++ practice.js
@@ -7,7 +7,12 @@
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
   //Code here
-//made some changes.
+var me = {
+  name: 'Brandon',
+  age: 31
+}
+alert(me.name);
+
 
 
 
